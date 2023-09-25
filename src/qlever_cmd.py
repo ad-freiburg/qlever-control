36b8bb6ef511a218acc62dbe7b3fe63d5ba93e1f
Mahmoud Khalaf <mahmoud.khalaf@students.uni-freiburg.de>
2023-09-25 04:47:05 +0200

    mimic previous args

--- src/qlever_cmd.py
+++ src/qlever_cmd.py
@@ -1,6 +1,8 @@
 import argparse
 import argcomplete
 
+from src.qlever_config import QleverConfig
+
 VERSION = "0.0.1"
 
 
@@ -13,43 +15,72 @@ parser = argparse.ArgumentParser(
     epilog="© 2023 University of Freiburg, Chair for Algorithms and Data Structures",
     allow_abbrev=True,
     exit_on_error=True,
-    
 )
 
 parser.add_argument("--version", action="version", version="%(prog)s " + VERSION)
-parser.add_argument(
-    "--health", action="store_true", help="list particulars of the installation",
-)
-parser.add_argument("--autocomplete", action="store_true", help="auto completion in the command line")
+# parser.add_argument(
+#     "-c",
+#     "--config",
+#     metavar="CONFIG",
+#     help="load from a bundled Qleverfile",
+# )
 
-parser.add_argument("--lsconfigs", action="store_true", help="list all bundled")
-parser.add_argument("-c", "--config", help="load from a bundled Qleverfile")
 
-parser_cmd = parser.add_subparsers(
-    title="Supported runtime(s)",
-    description="You can interact with QLever using one of the following:",
-    help="interact with QLever with Docker or Natively",
-    # required=True,
-)
-
-parser_cmd_docker = parser_cmd.add_parser(
-    "docker", help="use Docker to index datasets and run server"
-)
-parser_cmd_docker.add_argument(
-    "docker_action",
-    choices=["up", "down", "stats"],
-    help="Spawn, stop or show stats of a Qlever container"
+group_options = parser.add_argument_group(
+    "Qlever Options",
+    description="Sanity checks, version info, autocomplete, etc...",
+    # help="Checking version, health status, system installation, etc."
 )
 
-parser_cmd_native = parser_cmd.add_parser(
-    "native", help="use precompiled native executables"
+
+group_options.add_argument(
+    "--health",
+    action="store_true",
+    help="list particulars of the installation",
+)
+group_options.add_argument(
+    "--autocomplete",
+    action="store_true",
+    help="auto completion in the command line",
+)
+group_options.add_argument(
+    "--lsconfigs", action="store_true", help="list all bundled Qleverfiles"
 )
 
-parser_cmd_native.add_argument(
-    "native_action",
-    choices=["start", "stop"],
-    help="Start or Stop ServerMain"
+group_options.add_argument(
+    "--setup-config",
+    # action="store_true",
+    choices=QleverConfig.show_available_config_names(),  # default bundled configs
+    default="Qleverfile.default",
+    metavar="Qle..",
+    help="create a new config in the current dir",
+)
 
+group_qlever_action = parser.add_argument_group(
+    title="Qlever Actions",
+    description="Execute Qlever actions against selected config",
+).add_mutually_exclusive_group()
+
+
+group_qlever_action.add_argument(
+    "-g",
+    "--get-data",
+    action="store_true",
+    help="downloads .zip file of size X MB, uncompressed to Y MB",
+)
+
+group_qlever_action.add_argument(
+    "-i",
+    "--index",
+    action="store_true",
+    help="takes ~X seconds and ~Y GB RAM (on an AMD Ryzen 9 5900X)",
+)
+
+group_qlever_action.add_argument(
+    "-s",
+    "--start",
+    action="store_true",
+    help="starts the server (instant)",
 )
 
 

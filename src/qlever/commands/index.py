a9cf3ae82b29982ba8bd6a675f9247c91beeaf98
Elton Soares <eltonfss@gmail.com>
2024-06-21 11:25:05 -0300

    Update index.py
    Setting soft limit (ulimit -Sn) might fail if hard limit is lower (ulimit -Hn). Also, since ";" separator was used the error was only detected at the end of the IndexBuilderMain execution. Now both limits will be set and the IndexBuilderMain will only run if the ulimit succeeds.

--- src/qlever/commands/index.py
+++ src/qlever/commands/index.py
@@ -64,7 +64,7 @@ class IndexCommand(QleverCommand):
         total_file_size = get_total_file_size(
                 shlex.split(args.input_files))
         if total_file_size > 1e10:
-            index_cmd = f"ulimit -Sn 1048576; {index_cmd}"
+            index_cmd = f"ulimit -Hn 1048576 && ulimit -Sn 1048576 && {index_cmd}"
 
         # Run the command in a container (if so desired).
         if args.system in Containerize.supported_systems():

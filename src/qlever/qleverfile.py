0318a9e21ad1944b0adfa22ed73c989ade31d28f
Julian Mundhahs <julian.mundhahs@gmail.com>
2024-08-11 11:44:19 +0200

    Change default UI port to 9000 Port 7000 is used by default on Mac for AirPlay

--- src/qlever/qleverfile.py
+++ src/qlever/qleverfile.py
@@ -173,7 +173,7 @@ class Qleverfile:
                 help="The name of the container used by `qlever start`")
 
         ui_args["ui_port"] = arg(
-                "--ui_port", type=int, default=7000,
+                "--ui_port", type=int, default=9000,
                 help="The port of the Qlever UI when running `qlever ui`")
         ui_args["ui_config"] = arg(
                 "--ui-config", type=str, default="default",

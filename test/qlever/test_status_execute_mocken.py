ac200b0af42448ccce9e3d9d1a48fbf06d8eabf5
SimonL22 <simon2.lempp@freenet.de>
2024-08-14 13:09:26 +0200

    Update test_status_execute_mocken.py

--- test/qlever/test_status_execute_mocken.py
+++ test/qlever/test_status_execute_mocken.py
@@ -11,17 +11,20 @@ class TestStatusCommand(unittest.TestCase):
     # Test the execute function while mocking psutil.process_iter and show_process_info
     @patch('qlever.util.show_process_info')
     @patch('psutil.process_iter')
-    def test_execute(self, mock_process_iter, mock_show_process_info):
+    def test_execute_processes_found(self, mock_process_iter, mock_show_process_info):
         args = MagicMock()
         args.cmdline_regex = "^(ServerMain|IndexBuilderMain)"
         args.show = False
+        
         # replaces return of psutil.process_iter with simplified objects.
         # For other testing use more realistic objects: {'name': 'systemd', 'pid': 1, 'username': 'root'}
         mock_process_iter.return_value = ['process1.anders', 'process2.anders', 'process3.anders', 'example2.qlever']
         mock_show_process_info.side_effect = [False, False, False, True]
         sc = StatusCommand
+        
         # execute the execute function and saving the result
         result = sc.execute(args)
+        
         # Assert
         mock_process_iter.assert_called_once()
         mock_show_process_info.assert_called_once_with(mock_process_iter, args.cmdline_regex, show_heading=True)
@@ -33,17 +36,17 @@ class TestStatusCommand(unittest.TestCase):
         args.cmdline_regex = "^(ServerMain|IndexBuilderMain)"
         args.show = False
 
-        # Mock process_iter to return an empty list, simulating no matching processes
+        # Mock process_iter to return an empty list, simulating that no matching processes are found
         mock_process_iter.return_value = []
 
-        # Redirect stdout to capture print output
+        # Capture the string-output
         captured_output = StringIO()
         sys.stdout = captured_output
 
         # Instantiate the StatusCommand
         status_command = StatusCommand()
 
-        # Act
+        # execute the execute function and saving the result
         result = status_command.execute(args)
 
         # Reset redirect.

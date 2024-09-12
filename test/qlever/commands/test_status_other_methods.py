f6c759d91306936c82e5fff4c85b55536807329a
SimonL22 <simon2.lempp@freenet.de>
2024-09-12 14:31:18 +0200

    Update test_status_other_methods.py

--- test/qlever/commands/test_status_other_methods.py
+++ test/qlever/commands/test_status_other_methods.py
@@ -6,11 +6,11 @@ import argparse
 class TestStatusCommand(unittest.TestCase):
     def test_description(self):
         result = StatusCommand().description()
-        self.assertEqual(result, "Show QLever processes running on this machine")
+        self.assertEqual(result,
+                    "Show QLever processes running on this machine")
 
     def test_should_have_qleverfile(self):
         self.assertFalse(StatusCommand().should_have_qleverfile())
-
 
     def test_relevant_qleverfile_arguments(self):
         result = StatusCommand().relevant_qleverfile_arguments()
@@ -29,8 +29,10 @@ class TestStatusCommand(unittest.TestCase):
         args = parser.parse_args([])
 
         # Test that the default value is set correctly
-        self.assertEqual(args.cmdline_regex, "^(ServerMain|IndexBuilderMain)")
+        self.assertEqual(args.cmdline_regex, 
+                         "^(ServerMain|IndexBuilderMain)")
 
         # Test that the help text is correctly set
         argument_help = subparser._group_actions[-1].help
-        self.assertEqual(argument_help, "Show only processes where the command line matches this regex")
+        self.assertEqual(argument_help,
+        "Show only processes where the command line matches this regex")

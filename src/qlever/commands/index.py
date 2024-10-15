ad-freiburg/qlever-control

commit d0e78e26324fb00c1d876aaaabf35207eb6cd698

Add Qlevefile variable FORMAT and support NQ (#59)
So far, the only input formats supported were NTriples and Turtle. In QLever, they both use the same (Turtle) parser, so the `qlever` script always called `IndexBuilderMain` with option `-F ttl`. Since we are now approaching support of named graphs, add a config variable `FORMAT` with the options `nt`, `ttl`, *and* `nq`, which then become the argument of the `-F` option in the call to `IndexBuilderMain`.

src/qlever/commands/index.py
@@ -25,7 +25,7 @@ class IndexCommand(QleverCommand):
         return True
 
     def relevant_qleverfile_arguments(self) -> dict[str: list[str]]:
-        return {"data": ["name"],
+        return {"data": ["name", "format"],
                 "index": ["input_files", "cat_input_files", "settings_json",
                           "index_binary",
                           "only_pso_and_pos_permutations", "use_patterns",
@@ -41,7 +41,7 @@ class IndexCommand(QleverCommand):
     def execute(self, args) -> bool:
         # Construct the command line.
         index_cmd = (f"{args.cat_input_files} | {args.index_binary}"
-                     f" -F ttl -f -"
+                     f" -F {args.format} -"
                      f" -i {args.name}"
                      f" -s {args.name}.settings.json")
         if args.only_pso_and_pos_permutations:

src/qlever/qleverfile.py
@@ -53,6 +53,10 @@ class Qleverfile:
                 "--text-description", type=str, default=None,
                 help="A concise description of the additional text data"
                      " if any")
+        data_args["format"] = arg(
+                "--format", type=str, default="ttl",
+                choices=["ttl", "nt", "nq"],
+                help="The format of the data")
 
         index_args["input_files"] = arg(
                 "--input-files", type=str, required=True,

commit 466df71d110135925439afc26da1b27d931c823a

Fix bug introduced by commit d0e78e26
In commit d0e78e26, the argument `-f -` to IndexBuilderMain was
erroneously replaced by `-`. This is now reversed.

pyproject.toml
@@ -5,7 +5,7 @@ build-backend = "setuptools.build_meta"
 [project]
 name = "qlever"
 description = "Script for using the QLever SPARQL engine."
-version = "0.5.7"
+version = "0.5.8"
 authors = [
     { name = "Hannah Bast", email = "bast@cs.uni-freiburg.de" }
 ]

src/qlever/commands/index.py
@@ -41,7 +41,7 @@ class IndexCommand(QleverCommand):
     def execute(self, args) -> bool:
         # Construct the command line.
         index_cmd = (f"{args.cat_input_files} | {args.index_binary}"
-                     f" -F {args.format} -"
+                     f" -F {args.format} -f -"
                      f" -i {args.name}"
                      f" -s {args.name}.settings.json")
         if args.only_pso_and_pos_permutations:
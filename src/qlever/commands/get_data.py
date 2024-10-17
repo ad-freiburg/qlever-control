9264e53172da3e9174a80b5359421d09d189ac13
Hannah Bast <bast@cs.uni-freiburg.de>
2024-10-17 17:23:49 +0200

    Autoformat code using `black`

--- src/qlever/commands/get_data.py
+++ src/qlever/commands/get_data.py
@@ -21,7 +21,7 @@ class GetDataCommand(QleverCommand):
     def should_have_qleverfile(self) -> bool:
         return True
 
-    def relevant_qleverfile_arguments(self) -> dict[str: list[str]]:
+    def relevant_qleverfile_arguments(self) -> dict[str : list[str]]:
         return {"data": ["name", "get_data_cmd"], "index": ["input_files"]}
 
     def additional_arguments(self, subparser) -> None:
@@ -37,12 +37,11 @@ class GetDataCommand(QleverCommand):
         try:
             run_command(args.get_data_cmd, show_output=True)
         except Exception as e:
-            log.error(f"Problem executing \"{args.get_data_cmd}\": {e}")
+            log.error(f'Problem executing "{args.get_data_cmd}": {e}')
             return False
 
         # Show the total file size in GB and return.
         patterns = shlex.split(args.input_files)
         total_file_size = get_total_file_size(patterns)
-        print(f"Download successful, total file size: "
-              f"{total_file_size:,} bytes")
+        print(f"Download successful, total file size: " f"{total_file_size:,} bytes")
         return True

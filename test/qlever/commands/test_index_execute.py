b56199b9d5ce954d80a7c40dbbd63503e14f9a87
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2024-12-23 10:59:47 +0100

    formatting

--- test/qlever/commands/test_index_execute.py
+++ test/qlever/commands/test_index_execute.py
@@ -1,20 +1,27 @@
 from __future__ import annotations
-import unittest
-from unittest.mock import patch, MagicMock, call
-from qlever.commands.index import IndexCommand
+
 import shlex
+import unittest
+from unittest.mock import MagicMock, call, patch
+
+from qlever.commands.index import IndexCommand
 
 
 # Test execute of index command for basic case with successful execution
 class TestIndexCommand(unittest.TestCase):
-    @patch('qlever.commands.index.run_command')
-    @patch('qlever.commands.index.Containerize')
-    @patch('qlever.commands.index.get_existing_index_files')
-    @patch('qlever.commands.index.get_total_file_size')
-    @patch('qlever.commands.index.glob')
-    def test_execute_successful_indexing_without_extras(self, mock_glob,
-                    mock_get_total_file_size, mock_get_existing_index_files,
-                                         mock_containerize, mock_run_command):
+    @patch("qlever.commands.index.run_command")
+    @patch("qlever.commands.index.Containerize")
+    @patch("qlever.commands.index.get_existing_index_files")
+    @patch("qlever.commands.index.get_total_file_size")
+    @patch("qlever.commands.index.glob")
+    def test_execute_successful_indexing_without_extras(
+        self,
+        mock_glob,
+        mock_get_total_file_size,
+        mock_get_existing_index_files,
+        mock_containerize,
+        mock_run_command,
+    ):
         # Setup args
         args = MagicMock()
         args.name = "TestName"
@@ -43,40 +50,53 @@ class TestIndexCommand(unittest.TestCase):
         mock_run_command.return_value = None
         mock_containerize.supported_systems.return_value = ["docker"]
 
-
         # Instantiate and executing the IndexCommand
         result = IndexCommand().execute(args)
 
         # Assertions
-        expected_index_cmd = (f"{args.cat_input_files} | {args.index_binary}"
-                         f" -i {args.name} -s {args.name}.settings.json"
-                         f" -F {args.format} -f - | tee"
-                         f" {args.name}.index-log.txt")
+        expected_index_cmd = (
+            f"{args.cat_input_files} | {args.index_binary}"
+            f" -i {args.name} -s {args.name}.settings.json"
+            f" -F {args.format} -f - | tee"
+            f" {args.name}.index-log.txt"
+        )
         index_cmd_call = call(expected_index_cmd, show_output=True)
 
-        expected_settings_json_cmd = (f"echo {shlex.quote(args.settings_json)} "
-                             f"> {args.name}.settings.json")
+        expected_settings_json_cmd = (
+            f"echo {shlex.quote(args.settings_json)} "
+            f"> {args.name}.settings.json"
+        )
 
         expected_index_binary_cmd = f"{args.index_binary} --help"
 
         # Testing if run_command was called exactly 3 times with the correct
         # parameters and in the correct order
-        mock_run_command.assert_has_calls([call(expected_index_binary_cmd),
-                                call(expected_settings_json_cmd), 
-                                index_cmd_call], any_order=False)
+        mock_run_command.assert_has_calls(
+            [
+                call(expected_index_binary_cmd),
+                call(expected_settings_json_cmd),
+                index_cmd_call,
+            ],
+            any_order=False,
+        )
         assert result
 
-
     # Test execute for file already existing
-    @patch('qlever.commands.index.run_command')
-    @patch('qlever.commands.index.Containerize')
-    @patch('qlever.commands.index.get_existing_index_files')
-    @patch('qlever.commands.index.get_total_file_size')
-    @patch('qlever.commands.index.log')
-    @patch('qlever.commands.index.glob')
-    def test_execute_indexing_with_already_existing_files(self, mock_glob,
-         mock_log, mock_get_total_file_size, mock_get_existing_index_files,
-         mock_containerize, mock_run_command):
+    @patch("qlever.commands.index.run_command")
+    @patch("qlever.commands.index.Containerize")
+    @patch("qlever.commands.index.get_existing_index_files")
+    @patch("qlever.commands.index.get_total_file_size")
+    @patch("qlever.commands.index.log")
+    @patch("qlever.commands.index.glob")
+    def test_execute_indexing_with_already_existing_files(
+        self,
+        mock_glob,
+        mock_log,
+        mock_get_total_file_size,
+        mock_get_existing_index_files,
+        mock_containerize,
+        mock_run_command,
+    ):
         # Setup args
         args = MagicMock()
         args.name = "TestName"
@@ -104,7 +124,6 @@ class TestIndexCommand(unittest.TestCase):
         mock_run_command.return_value = None
         mock_containerize.supported_systems.return_value = []
 
-
         # Instantiate IndexCommand and execute the function
         result = IndexCommand().execute(args)
 
@@ -112,27 +131,33 @@ class TestIndexCommand(unittest.TestCase):
         assert not result
         # Verify that the error message was logged
         mock_log.error.assert_called_once_with(
-            f"Index files for basename \"{args.name}\" found, if you "
-                    f"want to overwrite them, use --overwrite-existing")
+            f'Index files for basename "{args.name}" found, if you '
+            f"want to overwrite them, use --overwrite-existing"
+        )
         # Check that the info log contains the exception message
         mock_log.info.assert_any_call(
-            f"Index files found: {mock_get_existing_index_files.return_value}")
+            f"Index files found: {mock_get_existing_index_files.return_value}"
+        )
         # Checking if run_command was only called once
         # (not after detecting existing files)
         mock_run_command.assert_called_once_with(f"{args.index_binary} --help")
 
-
     # Test execute for no index binary found
-    @patch('qlever.commands.index.run_command')
-    @patch('qlever.commands.index.Containerize')
-    @patch('qlever.commands.index.get_existing_index_files')
-    @patch('qlever.commands.index.get_total_file_size')
-    @patch('qlever.commands.index.log')
-    @patch('qlever.commands.index.glob')
-    def test_execute_fails_if_no_indexing_binary_is_found(self, mock_glob,
-                            mock_log, mock_get_total_file_size,
-                            mock_get_existing_index_files, mock_containerize,
-                            mock_run_command):
+    @patch("qlever.commands.index.run_command")
+    @patch("qlever.commands.index.Containerize")
+    @patch("qlever.commands.index.get_existing_index_files")
+    @patch("qlever.commands.index.get_total_file_size")
+    @patch("qlever.commands.index.log")
+    @patch("qlever.commands.index.glob")
+    def test_execute_fails_if_no_indexing_binary_is_found(
+        self,
+        mock_glob,
+        mock_log,
+        mock_get_total_file_size,
+        mock_get_existing_index_files,
+        mock_containerize,
+        mock_run_command,
+    ):
         # Setup args
         args = MagicMock()
         args.name = "TestName"
@@ -168,26 +193,32 @@ class TestIndexCommand(unittest.TestCase):
         self.assertFalse(result)
         # Verify that the error message was logged
         mock_log.error.assert_called_once_with(
-            f"Running \"{args.index_binary}\" failed, "
-                          f"set `--index-binary` to a different binary or "
-                          f"set `--system to a container system`")
+            f'Running "{args.index_binary}" failed, '
+            f"set `--index-binary` to a different binary or "
+            f"set `--system to a container system`"
+        )
         # Check that the info log contains the exception message
         mock_log.info.assert_any_call(
-            "The error message was: Binary not found")
+            "The error message was: Binary not found"
+        )
         # Checking if run_command was only called once
         # (not after throwing an Exception)
         mock_run_command.assert_called_once_with(f"{args.index_binary} --help")
 
-
     # Test execute for file size > 10gb
-    @patch('qlever.commands.index.run_command')
-    @patch('qlever.commands.index.Containerize')
-    @patch('qlever.commands.index.get_existing_index_files')
-    @patch('qlever.commands.index.get_total_file_size')
-    @patch('qlever.commands.index.glob')
-    def test_execute_total_file_size_greater_than_ten_gb(self, mock_glob,
-                                                    mock_get_total_file_size,
-         mock_get_existing_index_files, mock_containerize, mock_run_command):
+    @patch("qlever.commands.index.run_command")
+    @patch("qlever.commands.index.Containerize")
+    @patch("qlever.commands.index.get_existing_index_files")
+    @patch("qlever.commands.index.get_total_file_size")
+    @patch("qlever.commands.index.glob")
+    def test_execute_total_file_size_greater_than_ten_gb(
+        self,
+        mock_glob,
+        mock_get_total_file_size,
+        mock_get_existing_index_files,
+        mock_containerize,
+        mock_run_command,
+    ):
         # Setup args
         args = MagicMock()
         args.name = "TestName"
@@ -222,19 +253,17 @@ class TestIndexCommand(unittest.TestCase):
         # Assertions
         expected_index_cmd = (
             f"ulimit -Sn 1048576; {args.cat_input_files} | {args.index_binary}"
-                         f" -i {args.name} -s {args.name}.settings.json"
-                         f" -F {args.format} -f -"
+            f" -i {args.name} -s {args.name}.settings.json"
+            f" -F {args.format} -f -"
             f" | tee {args.name}.index-log.txt"
         )
         mock_run_command.assert_any_call(expected_index_cmd, show_output=True)
         self.assertTrue(result)
 
-
     # Test elif branch for multi_input_json
-    @patch('qlever.commands.index.log')
-    @patch('qlever.commands.index.json')
-    def test_execute_get_input_options_error(self, mock_json,
-                                                    mock_log):
+    @patch("qlever.commands.index.log")
+    @patch("qlever.commands.index.json")
+    def test_execute_get_input_options_error(self, mock_json, mock_log):
         # Setup args
         args = MagicMock()
         args.cat_input_files = False
@@ -246,20 +275,22 @@ class TestIndexCommand(unittest.TestCase):
         # Instantiate IndexCommand and execute the function
         result = IndexCommand().execute(args)
 
-        error_msg = ("Failed to parse `MULTI_INPUT_JSON` as either JSON or "
-                     "JSONL (Wrong format)")
+        error_msg = (
+            "Failed to parse `MULTI_INPUT_JSON` as either JSON or "
+            "JSONL (Wrong format)"
+        )
         # Asserts
         # Verify that the error message was logged
         mock_log.error.assert_called_once_with(error_msg)
         # Assert that log_info was called exactly 2 times with the
         # correct arguments in order
-        mock_log.info.assert_has_calls([call(""), call(args.multi_input_json)],
-                                       any_order=False)
+        mock_log.info.assert_has_calls(
+            [call(""), call(args.multi_input_json)], any_order=False
+        )
         assert not result
 
-
     # Test else branch for multi_input_json
-    @patch('qlever.commands.index.log')
+    @patch("qlever.commands.index.log")
     def test_execute_cat_files_and_multi_json(self, mock_log):
         # Setup args
         args = MagicMock()
@@ -269,25 +300,28 @@ class TestIndexCommand(unittest.TestCase):
         # Instantiate IndexCommand and execute the function
         result = IndexCommand().execute(args)
 
-        error_msg = ("Specify exactly one of `CAT_INPUT_FILES` (for a "
-                      "single input stream) or `MULTI_INPUT_JSON` (for "
-                      "multiple input streams)")
+        error_msg = (
+            "Specify exactly one of `CAT_INPUT_FILES` (for a "
+            "single input stream) or `MULTI_INPUT_JSON` (for "
+            "multiple input streams)"
+        )
         log_msg = "See `qlever index --help` for more information"
         # Asserts
         # Verify that the error message was logged
         mock_log.error.assert_called_once_with(error_msg)
         # Assert that log_info was called exactly 2 times with the
         # correct arguments in order
-        mock_log.info.assert_has_calls([call(""), call(log_msg)],
-                                       any_order=False)
+        mock_log.info.assert_has_calls(
+            [call(""), call(log_msg)], any_order=False
+        )
         assert not result
 
-
     # Tests all the extra additions to the index_cmd and the show option
-    @patch('qlever.commands.index.IndexCommand.get_input_options_for_json')
-    @patch('qlever.commands.index.IndexCommand.show')
-    def test_execute_successful_indexing_with_extras_and_show(self, mock_show,
-                                                mock_input_json):
+    @patch("qlever.commands.index.IndexCommand.get_input_options_for_json")
+    @patch("qlever.commands.index.IndexCommand.show")
+    def test_execute_successful_indexing_with_extras_and_show(
+        self, mock_show, mock_input_json
+    ):
         # Setup args
         args = MagicMock()
         args.name = "TestName"
@@ -310,19 +344,25 @@ class TestIndexCommand(unittest.TestCase):
         result = IndexCommand().execute(args)
 
         # Assertions
-        expected_index_cmd = (f"{args.index_binary}"
-                     f" -i {args.name} -s {args.name}.settings.json"
-                     f" {mock_input_json.return_value}"
-                     f" --only-pso-and-pos-permutations --no-patterns"
-                     f" --no-patterns -w {args.name}.wordsfile.tsv"
-                     f" -d {args.name}.docsfile.tsv"
-                     f" --text-words-from-literals"
-                     f" --stxxl-memory {args.stxxl_memory}"
-                     f" | tee {args.name}.index-log.txt")
-        settings_json_cmd = (f"echo {shlex.quote(args.settings_json)} "
-                             f"> {args.name}.settings.json")
+        expected_index_cmd = (
+            f"{args.index_binary}"
+            f" -i {args.name} -s {args.name}.settings.json"
+            f" {mock_input_json.return_value}"
+            f" --only-pso-and-pos-permutations --no-patterns"
+            f" --no-patterns -w {args.name}.wordsfile.tsv"
+            f" -d {args.name}.docsfile.tsv"
+            f" --text-words-from-literals"
+            f" --stxxl-memory {args.stxxl_memory}"
+            f" | tee {args.name}.index-log.txt"
+        )
+        settings_json_cmd = (
+            f"echo {shlex.quote(args.settings_json)} "
+            f"> {args.name}.settings.json"
+        )
 
         # Verify that show was called with the right parameters
-        mock_show.assert_called_once_with(f"{settings_json_cmd}\n"
-                                f"{expected_index_cmd}", only_show = args.show)
+        mock_show.assert_called_once_with(
+            f"{settings_json_cmd}\n" f"{expected_index_cmd}",
+            only_show=args.show,
+        )
         assert result
--- test/qlever/commands/test_index_other_methods.py
+++ test/qlever/commands/test_index_other_methods.py
@@ -1,36 +1,35 @@
+import argparse
 import unittest
 from unittest.mock import MagicMock, patch
+
 from qlever.commands.index import IndexCommand
-import argparse
 
 
 class TestIndexCommand(unittest.TestCase):
-
     def setUp(self):
         # Initialize an IndexCommand instance to access the helper function
         self.index_command = IndexCommand()
 
     def test_description(self):
-
         # Call the method
         result = self.index_command.description()
 
-        self.assertEqual(result, "Build the index for a given "
-                                 "RDF dataset")
+        self.assertEqual(result, "Build the index for a given " "RDF dataset")
 
     def test_should_have_qleverfile(self):
-
         # Call the method
         result = self.index_command.should_have_qleverfile()
 
         assert result
 
     def test_relevant_qleverfile_arguments(self):
-
         # Call the method
         result = self.index_command.relevant_qleverfile_arguments()
 
-        self.assertEqual(result, {"data": ["name", "format"],
+        self.assertEqual(
+            result,
+            {
+                "data": ["name", "format"],
                 "index": [
                     "input_files",
                     "cat_input_files",
@@ -43,13 +42,14 @@ class TestIndexCommand(unittest.TestCase):
                     "text_index",
                     "stxxl_memory",
                 ],
-                "runtime": ["system", "image", "index_container"]})
+                "runtime": ["system", "image", "index_container"],
+            },
+        )
 
     def test_additional_arguments(self):
-
         # Create a parser and a subparser
         parser = argparse.ArgumentParser()
-        subparser = parser.add_argument_group('test')
+        subparser = parser.add_argument_group("test")
 
         # Call the method
         self.index_command.additional_arguments(subparser)
@@ -61,24 +61,29 @@ class TestIndexCommand(unittest.TestCase):
 
         # Test that the help text for cmdline_regex is correctly set
         argument_help = subparser._group_actions[-1].help
-        self.assertEqual(argument_help, "Overwrite an existing index, "
-                                        "think twice before using.")
+        self.assertEqual(
+            argument_help,
+            "Overwrite an existing index, " "think twice before using.",
+        )
 
     def test_get_input_options_for_json_valid_input(self):
         # Mock args with a valid multi_input_json and format
         args = MagicMock()
-        args.multi_input_json = ('[{"cmd": "test_data1", "format": "json"}, '
-                                 '{"cmd": "test_data2"}]')
+        args.multi_input_json = (
+            '[{"cmd": "test_data1", "format": "json"}, '
+            '{"cmd": "test_data2"}]'
+        )
         args.format = "jsonld"  # Default format if not specified in JSON
 
         result = self.index_command.get_input_options_for_json(args)
 
         # Expected command-line options string based on the JSON data
-        expected_result = ('-f <(test_data1) -g - -F json -f '
-                           '<(test_data2) -g - -F jsonld')
+        expected_result = (
+            "-f <(test_data1) -g - -F json -f " "<(test_data2) -g - -F jsonld"
+        )
         self.assertEqual(result, expected_result)
 
-    @patch('qlever.commands.index.json')
+    @patch("qlever.commands.index.json")
     def test_get_input_options_for_json_invalid_format(self, mock_json):
         # Mock args with invalid JSON format
         args = MagicMock()
@@ -96,11 +101,10 @@ class TestIndexCommand(unittest.TestCase):
         self.assertEqual(
             context.exception.error_message,
             "Failed to parse `MULTI_INPUT_JSON` as either JSON "
-            "or JSONL (Wrong format)"
+            "or JSONL (Wrong format)",
         )
         self.assertEqual(
-            context.exception.additional_info,
-            args.multi_input_json
+            context.exception.additional_info, args.multi_input_json
         )
 
     def test_get_input_options_for_json_no_array(self):
@@ -114,14 +118,16 @@ class TestIndexCommand(unittest.TestCase):
         # Verify error message mentions array requirement
         self.assertEqual(
             "Element 0 in `MULTI_INPUT_JSON` must contain a key `cmd`",
-            context.exception.error_message)
-        self.assertEqual({"key": "test_data1"},
-                         context.exception.additional_info)
+            context.exception.error_message,
+        )
+        self.assertEqual(
+            {"key": "test_data1"}, context.exception.additional_info
+        )
 
     def test_get_input_options_for_json_empty(self):
         # Mock args with an empty JSON array
         args = MagicMock()
-        args.multi_input_json = '[]'
+        args.multi_input_json = "[]"
 
         with self.assertRaises(IndexCommand.InvalidInputJson) as context:
             self.index_command.get_input_options_for_json(args)
@@ -129,45 +135,50 @@ class TestIndexCommand(unittest.TestCase):
         # Verify error message mentions non-empty requirement
         self.assertEqual(
             context.exception.error_message,
-            "`MULTI_INPUT_JSON` must contain at least one element"
+            "`MULTI_INPUT_JSON` must contain at least one element",
         )
-        self.assertEqual(context.exception.additional_info,
-                         args.multi_input_json)
+        self.assertEqual(
+            context.exception.additional_info, args.multi_input_json
+        )
 
     def test_get_input_options_for_json_object_structure(self):
         # Mock args where one of the JSON objects is missing the
         # required "cmd" key
         args = MagicMock()
-        args.multi_input_json = ('[{"cmd": "test_data1", "format": "json"}, '
-                                 '{"format": "json2"}]')  # Missing "cmd"
+        args.multi_input_json = (
+            '[{"cmd": "test_data1", "format": "json"}, ' '{"format": "json2"}]'
+        )  # Missing "cmd"
 
         with self.assertRaises(IndexCommand.InvalidInputJson) as context:
             self.index_command.get_input_options_for_json(args)
 
         # Verify error mentions the missing `cmd` key
-        self.assertEqual(  "Element 1 in `MULTI_INPUT_JSON` must contain "
-                           "a key `cmd`",
-            context.exception.error_message)
+        self.assertEqual(
+            "Element 1 in `MULTI_INPUT_JSON` must contain " "a key `cmd`",
+            context.exception.error_message,
+        )
 
         self.assertEqual(
-            {"format": "json2"}, context.exception.additional_info)
+            {"format": "json2"}, context.exception.additional_info
+        )
 
     def test_get_input_options_for_json_object_type(self):
         # Mock args where one of the JSON objects is not a dictionary
         args = MagicMock()
-        args.multi_input_json = ('[{"cmd": "test_data1", "format": "json"}, '
-                                 '5]')  # Missing "cmd"
+        args.multi_input_json = (
+            '[{"cmd": "test_data1", "format": "json"}, ' "5]"
+        )  # Missing "cmd"
 
         with self.assertRaises(IndexCommand.InvalidInputJson) as context:
             self.index_command.get_input_options_for_json(args)
 
         # Verify error mentions the missing `cmd` key
-        self.assertEqual("Element 1 in `MULTI_INPUT_JSON` must be a JSON "
-                        "object",
-                         context.exception.error_message)
-
         self.assertEqual(
-            5, context.exception.additional_info)
+            "Element 1 in `MULTI_INPUT_JSON` must be a JSON " "object",
+            context.exception.error_message,
+        )
+
+        self.assertEqual(5, context.exception.additional_info)
 
     def test_get_input_options_for_json_extra_keys(self):
         # Mock args where one of the JSON objects contains an extra key
@@ -178,10 +189,14 @@ class TestIndexCommand(unittest.TestCase):
             self.index_command.get_input_options_for_json(args)
 
         # Verify error mentions the missing `cmd` key
-        self.assertEqual("Element 0 in `MULTI_INPUT_JSON` must only "
-                        "contain the keys `format`, `graph`, and `parallel`. "
-                        "Contains extra keys {'test_key'}.",
-                        context.exception.error_message)
+        self.assertEqual(
+            "Element 0 in `MULTI_INPUT_JSON` must only "
+            "contain the keys `format`, `graph`, and `parallel`. "
+            "Contains extra keys {'test_key'}.",
+            context.exception.error_message,
+        )
 
-        self.assertEqual({"cmd": "test_data1", "test_key": "data2"},
-                         context.exception.additional_info)
+        self.assertEqual(
+            {"cmd": "test_data1", "test_key": "data2"},
+            context.exception.additional_info,
+        )

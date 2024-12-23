db42c93ba5e2d3e903a8d759c8d67be0c3fd165c
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2024-12-23 10:52:31 +0100

    move files

--- test/qlever/commands/test_stop_execute.py
+++ test/qlever/commands/test_stop_execute.py
@@ -5,15 +5,17 @@ from qlever.commands.stop import StopCommand
 
 
 class TestStopCommand(unittest.TestCase):
-
-    @patch('qlever.commands.stop.StatusCommand.execute')
-    @patch('psutil.process_iter')
-    @patch('qlever.containerize.Containerize.stop_and_remove_container')
-    @patch('qlever.commands.stop.StopCommand.show')
-    def test_execute_no_matching_processes_or_containers(self, mock_show,
-                                                mock_stop_and_remove_container,
-                                                mock_process_iter,
-                                                mock_status_execute):
+    @patch("qlever.commands.stop.StatusCommand.execute")
+    @patch("psutil.process_iter")
+    @patch("qlever.containerize.Containerize.stop_and_remove_container")
+    @patch("qlever.commands.stop.StopCommand.show")
+    def test_execute_no_matching_processes_or_containers(
+        self,
+        mock_show,
+        mock_stop_and_remove_container,
+        mock_process_iter,
+        mock_status_execute,
+    ):
         # Setup args
         args = MagicMock()
         args.cmdline_regex = "ServerMain.* -i [^ ]*%%NAME%%"
@@ -36,21 +38,24 @@ class TestStopCommand(unittest.TestCase):
 
         # Assertions
         mock_show.assert_called_once_with(
-            f'Checking for processes matching "{expected_regex}"',
-            only_show=False)
+            f'Checking for processes matching "{expected_regex}"', only_show=False
+        )
         mock_process_iter.assert_called_once()
         mock_stop_and_remove_container.assert_not_called()
         mock_status_execute.assert_called_once_with(args)
         self.assertTrue(result)
 
-    @patch('qlever.commands.stop.StatusCommand.execute')
-    @patch('psutil.process_iter')
-    @patch('qlever.containerize.Containerize.stop_and_remove_container')
-    @patch('qlever.commands.stop.StopCommand.show')
-    def test_execute_with_matching_process(self, mock_show,
-                                           mock_stop_and_remove_container,
-                                           mock_process_iter,
-                                           mock_status_execute):
+    @patch("qlever.commands.stop.StatusCommand.execute")
+    @patch("psutil.process_iter")
+    @patch("qlever.containerize.Containerize.stop_and_remove_container")
+    @patch("qlever.commands.stop.StopCommand.show")
+    def test_execute_with_matching_process(
+        self,
+        mock_show,
+        mock_stop_and_remove_container,
+        mock_process_iter,
+        mock_status_execute,
+    ):
         # Setup args
         args = MagicMock()
         args.cmdline_regex = "ServerMain.* -i [^ ]*%%NAME%%"
@@ -67,10 +72,10 @@ class TestStopCommand(unittest.TestCase):
         # to test with real psutil.process objects use this:
 
         mock_process.as_dict.return_value = {
-            'cmdline': ['ServerMain', '-i', '/some/path/TestName'],
-            'pid': 1234,
-            'username': 'test_user'
-            }
+            "cmdline": ["ServerMain", "-i", "/some/path/TestName"],
+            "pid": 1234,
+            "username": "test_user",
+        }
 
         mock_process_iter.return_value = [mock_process]
 
@@ -85,21 +90,25 @@ class TestStopCommand(unittest.TestCase):
 
         # Assertions
         mock_show.assert_called_once_with(
-            f'Checking for processes matching "{expected_regex}"',
-            only_show=False)
+            f'Checking for processes matching "{expected_regex}"', only_show=False
+        )
         mock_process_iter.assert_called_once()
         mock_stop_and_remove_container.assert_not_called()
         mock_process.kill.assert_called_once()
         mock_status_execute.assert_not_called()
         self.assertTrue(result)
 
-    @patch('qlever.commands.stop.StatusCommand.execute')
-    @patch('psutil.process_iter')
-    @patch('qlever.containerize.Containerize.stop_and_remove_container')
-    @patch('qlever.commands.stop.StopCommand.show')
-    def test_execute_with_containers(self, mock_show,
-                                     mock_stop_and_remove_container,
-                                     mock_process_iter, mock_status_execute):
+    @patch("qlever.commands.stop.StatusCommand.execute")
+    @patch("psutil.process_iter")
+    @patch("qlever.containerize.Containerize.stop_and_remove_container")
+    @patch("qlever.commands.stop.StopCommand.show")
+    def test_execute_with_containers(
+        self,
+        mock_show,
+        mock_stop_and_remove_container,
+        mock_process_iter,
+        mock_status_execute,
+    ):
         # Setup args
         args = MagicMock()
         args.cmdline_regex = "ServerMain.* -i [^ ]*%%NAME%%"
@@ -124,21 +133,24 @@ class TestStopCommand(unittest.TestCase):
         mock_show.assert_called_once_with(
             f'Checking for processes matching "{expected_regex}" and for'
             f' Docker container with name "{args.server_container}"',
-            only_show=False)
+            only_show=False,
+        )
         mock_process_iter.assert_not_called()
         mock_stop_and_remove_container.assert_called_once()
         mock_status_execute.assert_not_called()
         self.assertTrue(result)
 
-    @patch('qlever.commands.stop.StatusCommand.execute')
-    @patch('psutil.process_iter')
-    @patch('qlever.containerize.Containerize.stop_and_remove_container')
-    @patch('qlever.commands.stop.StopCommand.show')
-    def test_execute_with_no_containers_and_no_matching_process(self,
-                                                mock_show,
-                                                mock_stop_and_remove_container,
-                                                mock_process_iter,
-                                                mock_status_execute):
+    @patch("qlever.commands.stop.StatusCommand.execute")
+    @patch("psutil.process_iter")
+    @patch("qlever.containerize.Containerize.stop_and_remove_container")
+    @patch("qlever.commands.stop.StopCommand.show")
+    def test_execute_with_no_containers_and_no_matching_process(
+        self,
+        mock_show,
+        mock_stop_and_remove_container,
+        mock_process_iter,
+        mock_status_execute,
+    ):
         # Setup args
         args = MagicMock()
         args.cmdline_regex = "ServerMain.* -i [^ ]*%%NAME%%"
@@ -166,22 +178,26 @@ class TestStopCommand(unittest.TestCase):
         mock_show.assert_called_once_with(
             f'Checking for processes matching "{expected_regex}" and for'
             f' Docker container with name "{args.server_container}"',
-            only_show=False)
+            only_show=False,
+        )
         mock_process_iter.assert_called_once()
         mock_stop_and_remove_container.assert_called()
         mock_status_execute.assert_called_once_with(args)
         self.assertTrue(result)
 
-    @patch('qlever.commands.stop.StatusCommand.execute')
-    @patch('psutil.process_iter')
-    @patch('qlever.containerize.Containerize.stop_and_remove_container')
-    @patch('qlever.commands.stop.StopCommand.show')
-    @patch('qlever.commands.stop.show_process_info')
-    def test_execute_with_error_killing_process(self, mock_show_process_info,
-                                                mock_show,
-                                                mock_stop_and_remove_container,
-                                                mock_process_iter,
-                                                mock_status_execute):
+    @patch("qlever.commands.stop.StatusCommand.execute")
+    @patch("psutil.process_iter")
+    @patch("qlever.containerize.Containerize.stop_and_remove_container")
+    @patch("qlever.commands.stop.StopCommand.show")
+    @patch("qlever.commands.stop.show_process_info")
+    def test_execute_with_error_killing_process(
+        self,
+        mock_show_process_info,
+        mock_show,
+        mock_stop_and_remove_container,
+        mock_process_iter,
+        mock_status_execute,
+    ):
         # Setup args
         args = MagicMock()
         args.cmdline_regex = "ServerMain.* -i [^ ]*%%NAME%%"
@@ -196,16 +212,16 @@ class TestStopCommand(unittest.TestCase):
         # Creating mock psutil.Process objects with necessary attributes
         mock_process = MagicMock()
         mock_process.as_dict.return_value = {
-            'cmdline': ['ServerMain', '-i', '/some/path/TestName'],
-            'pid': 1234,
-            'create_time': 1234567890,
-            'memory_info': MagicMock(rss=1024 * 1024 * 512),
-            'username': 'test_user'
-            }
+            "cmdline": ["ServerMain", "-i", "/some/path/TestName"],
+            "pid": 1234,
+            "create_time": 1234567890,
+            "memory_info": MagicMock(rss=1024 * 1024 * 512),
+            "username": "test_user",
+        }
         mock_process_iter.return_value = [mock_process]
 
         # Mock process.kill to raise an exception
-        mock_process.kill.side_effect = Exception('Test')
+        mock_process.kill.side_effect = Exception("Test")
 
         # Instantiate the StopCommand
         sc = StopCommand()
@@ -215,13 +231,13 @@ class TestStopCommand(unittest.TestCase):
 
         # Assertions
         mock_show.assert_called_once_with(
-            f'Checking for processes matching "{expected_regex}"',
-            only_show=False)
+            f'Checking for processes matching "{expected_regex}"', only_show=False
+        )
         mock_process_iter.assert_called_once()
         mock_stop_and_remove_container.assert_not_called()
         mock_process.kill.assert_called_once()
-        mock_show_process_info.assert_called_once_with(mock_process, "",
-                                                       show_heading=True)
+        mock_show_process_info.assert_called_once_with(
+            mock_process, "", show_heading=True
+        )
         mock_status_execute.assert_not_called()
         self.assertFalse(result)
-

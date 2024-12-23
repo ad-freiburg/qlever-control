6ef67ed311ac5c606f24edbdf19fd7c9a3ec5417
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2024-12-23 10:11:38 +0100

    formatting

--- test/qlever/commands/test_status_execute.py
+++ test/qlever/commands/test_status_execute.py
@@ -14,47 +14,44 @@ def get_mock_args(only_show):
 
 
 class TestStatusCommand(unittest.TestCase):
-
-    @patch('qlever.commands.status.show_process_info')
-    @patch('psutil.process_iter')
+    @patch("qlever.commands.status.show_process_info")
+    @patch("psutil.process_iter")
     # testing execute for 2 processes. Just the second one is a qlever process.
-    # Mocking the process_iter and show_process_info method and testing 
+    # Mocking the process_iter and show_process_info method and testing
     # if the methods are called correctly.
-    def test_execute_processes_found(self, mock_process_iter,
-                                     mock_show_process_info):
+    def test_execute_processes_found(self, mock_process_iter, mock_show_process_info):
         # Mocking the input for the execute function
         [args, args.cmdline_regex, args.show] = get_mock_args(False)
 
         # Creating mock psutil.Process objects with necessary attributes
         mock_process1 = MagicMock()
-        mock_process1.as_dict.return_value = {'test': [1]}
+        mock_process1.as_dict.return_value = {"test": [1]}
         # to test with real psutil.process objects use this:
-        '''mock_process1.as_dict.return_value = {
+        """mock_process1.as_dict.return_value = {
             'cmdline': ['cmdline1'],
             'pid': 1,
             'username': 'user1',
             'create_time': datetime.now().timestamp(),
             'memory_info': MagicMock(rss=512 * 1024 * 1024)  # 512 MB
-        }'''
+        }"""
 
         mock_process2 = MagicMock()
-        mock_process2.as_dict.return_value = {'test': [2]}
+        mock_process2.as_dict.return_value = {"test": [2]}
         # to test with real psutil.process objects use this:
-        '''mock_process2.as_dict.return_value = {
+        """mock_process2.as_dict.return_value = {
             'cmdline': ['cmdline2'],
             'pid': 2,
             'username': 'user2',
             'create_time': datetime.now().timestamp(),
             'memory_info': MagicMock(rss=1024 * 1024 * 1024)  # 1 GB
-        }'''
+        }"""
 
         mock_process3 = MagicMock()
-        mock_process3.as_dict.return_value = {'test': [3]}
+        mock_process3.as_dict.return_value = {"test": [3]}
 
         # Mock the return value of process_iter
         # to be a list of these mocked process objects
-        mock_process_iter.return_value = [mock_process1, mock_process2,
-                                          mock_process3]
+        mock_process_iter.return_value = [mock_process1, mock_process2, mock_process3]
 
         # Simulate show_process_info returning False for the first
         # True for the second and False for the third process
@@ -73,15 +70,16 @@ class TestStatusCommand(unittest.TestCase):
         expected_calls = [
             call(mock_process1, args.cmdline_regex, show_heading=True),
             call(mock_process2, args.cmdline_regex, show_heading=True),
-            call(mock_process3, args.cmdline_regex, show_heading=False)]
-        mock_show_process_info.assert_has_calls(expected_calls,
-                                                any_order=False)
+            call(mock_process3, args.cmdline_regex, show_heading=False),
+        ]
+        mock_show_process_info.assert_has_calls(expected_calls, any_order=False)
         self.assertIsNone(result)
 
-    @patch('qlever.util.show_process_info')
-    @patch('psutil.process_iter')
-    def test_execute_no_processes_found(self, mock_process_iter,
-                                        mock_show_process_info):
+    @patch("qlever.util.show_process_info")
+    @patch("psutil.process_iter")
+    def test_execute_no_processes_found(
+        self, mock_process_iter, mock_show_process_info
+    ):
         # Mocking the input for the execute function
         [args, args.cmdline_regex, args.show] = get_mock_args(False)
 
@@ -114,7 +112,7 @@ class TestStatusCommand(unittest.TestCase):
         # Verify the correct output was printed
         self.assertIn("No processes found", captured_output.getvalue())
 
-    @patch.object(qlever.command.QleverCommand, 'show')
+    @patch.object(qlever.command.QleverCommand, "show")
     def test_execute_show_action_description(self, mock_show):
         # Mocking the input for the execute function
         [args, args.cmdline_regex, args.show] = get_mock_args(True)
@@ -123,9 +121,11 @@ class TestStatusCommand(unittest.TestCase):
         result = StatusCommand().execute(args)
 
         # Assert that verifies that show was called with the correct parameters
-        mock_show.assert_any_call(f"Show all processes on this machine where "
-                            f"the command line matches {args.cmdline_regex}"
-                            f" using Python's psutil library",
-                            only_show=args.show)
+        mock_show.assert_any_call(
+            f"Show all processes on this machine where "
+            f"the command line matches {args.cmdline_regex}"
+            f" using Python's psutil library",
+            only_show=args.show,
+        )
 
         self.assertFalse(result)

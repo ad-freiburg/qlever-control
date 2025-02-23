234743cb0e8d44cd46f918cb57544ee82c326924
Hannah Bast <bast@cs.uni-freiburg.de>
2025-02-23 14:22:28 +0100

    First working version of new `qlever ui` command
    The command now write a file `<name>.ui-config.yml`, which can be
    modified and reused.
    
    The next step is to also write `<name>.ui.db.sqlite3` (copy it from the
    image) and reuse it.

--- src/qlever/commands/ui.py
+++ src/qlever/commands/ui.py
@@ -12,6 +12,29 @@ from qlever.log import log
 from qlever.util import is_port_used, run_command
 
 
+# Return a YAML string for the given dictionary. Format values with
+# newlines using the "|" style.
+def dict_to_yaml(dictionary):
+    # Custom representer for yaml, which uses the "|" style only for
+    # multiline strings.
+    #
+    # NOTE: We eeplace all `\r\n` with `\n` because otherwise the `|` style
+    # does not work as expected.
+    class MultiLineDumper(yaml.Dumper):
+        def represent_scalar(self, tag, value, style=None):
+            value = value.replace("\r\n", "\n")
+            if isinstance(value, str) and "\n" in value:
+                style = "|"
+            return super().represent_scalar(tag, value, style)
+
+    # Dump as yaml.
+    return yaml.dump(
+        dictionary,
+        sort_keys=False,
+        Dumper=MultiLineDumper,
+    )
+
+
 class UiCommand(QleverCommand):
     """
     Class for launching the QLever UI web application.
@@ -46,11 +69,17 @@ class UiCommand(QleverCommand):
             "(default: <name>.ui-config.yml)",
         )
         subparser.add_argument(
-            "--pull-latest",
-            type=str,
-            choices=["yes", "no"],
-            default="yes",
-            help="Pull the latest image of the QLever UI",
+            "--no-pull-latest",
+            action="store_true",
+            default=False,
+            help="Do not pull the latest image for the QLever UI "
+            "(default: pull the latest image if image name contains '/')",
+        )
+        subparser.add_argument(
+            "--stop",
+            action="store_true",
+            default=False,
+            help="Stop the running container",
         )
 
     def execute(self, args) -> bool:
@@ -73,18 +102,20 @@ class UiCommand(QleverCommand):
                 log.info("")
 
         # Construct commands and show them.
+        pull_latest_image = "/" in args.ui_image and not args.no_pull_latest
         ui_config_name = args.name
         ui_config_file = args.ui_config_file or f"{args.name}.ui-config.yml"
-        server_url = f"http://{args.host_name}:{args.port}"
+        sparql_endpoint = f"http://{args.host_name}:{args.port}"
         ui_url = f"http://{args.host_name}:{args.ui_port}"
         pull_cmd = f"{args.ui_system} pull -q {args.ui_image}"
         get_config_cmd = (
             f"{args.ui_system} exec -it "
             f"{args.ui_image} "
-            f"bash -c \"python manage.py config {ui_config_name}\""
+            f'bash -c "python manage.py config {ui_config_name}"'
         )
         start_ui_cmd = (
             f"{args.ui_system} run -d "
+            f"--volume $(pwd):/db "
             f"--publish {args.ui_port}:7000 "
             f"--name {args.ui_container} "
             f"{args.ui_image}"
@@ -92,31 +123,35 @@ class UiCommand(QleverCommand):
         set_config_cmd = (
             f"{args.ui_system} exec -it "
             f"{args.ui_container} "
-            f"bash -c \"python manage.py config {ui_config_name} "
-            f"{ui_config_file} --hide-all-other-backends\""
+            f'bash -c "python manage.py config {ui_config_name} '
+            f'/db/{ui_config_file} --hide-all-other-backends"'
         )
-        self.show(
-            "\n".join(
-                [
-                    "Stop running containers",
-                    pull_cmd,
-                    get_config_cmd,
-                    start_ui_cmd,
-                    set_config_cmd,
-                ]
-            ),
-            only_show=args.show,
-        )
+        commands_to_show = []
+        commands_to_show.append("Check for running containers and stop them")
+        if not args.stop:
+            if pull_latest_image:
+                commands_to_show.append(pull_cmd)
+            commands_to_show.append(get_config_cmd)
+            commands_to_show.append(start_ui_cmd)
+            commands_to_show.append(set_config_cmd)
+        self.show("\n".join(commands_to_show), only_show=args.show)
         if qlever_is_running_in_container:
             return False
         if args.show:
             return True
 
         # Stop running containers.
+        was_found_and_stopped = False
         for container_system in Containerize.supported_systems():
-            Containerize.stop_and_remove_container(
+            was_found_and_stopped |= Containerize.stop_and_remove_container(
                 container_system, args.ui_container
             )
+        if was_found_and_stopped:
+            log.info(f"Stopped and removed container `{args.ui_container}`")
+        else:
+            log.info(f"No container with name `{args.ui_container}` found")
+        if args.stop:
+            return True
 
         # Check if the UI port is already being used.
         if is_port_used(args.ui_port):
@@ -128,8 +163,10 @@ class UiCommand(QleverCommand):
 
         # Start the QLever UI.
         try:
-            if args.pull_latest == "yes":
+            if pull_latest_image:
+                log.info(f"Pulling image `{args.ui_image}` for QLever UI")
                 run_command(pull_cmd)
+            log.info(f"Starting new container with name `{args.ui_container}`")
             run_command(start_ui_cmd)
         except subprocess.CalledProcessError as e:
             log.error(f"Failed to start the QLever UI ({e})")
@@ -138,17 +175,26 @@ class UiCommand(QleverCommand):
         # Check if config file with name `ui_config_file` exists. If not, try
         # to obtain it via `get_config_cmd` and set it as default.
         if Path(ui_config_file).exists():
-            log.info(f"Found config file `{ui_config_file}` for QLever UI ...")
+            log.info(f"Found config file `{ui_config_file}` and reusing it")
         else:
             log.info(
-                f"Trying to obtain default config for `{ui_config_name}` ..."
+                f"Checking if config `{ui_config_name}` exists in "
+                f"image `{args.ui_image}` "
             )
             try:
                 config_yaml = run_command(get_config_cmd, return_output=True)
+                log.info(
+                    f"Found it and setting URL for SPARQL endpoint to "
+                    f"{sparql_endpoint}"
+                )
                 config_dict = yaml.safe_load(config_yaml)
                 config_dict["config"]["backend"]["isDefault"] = True
-                config_dict["config"]["backend"]["baseUrl"] = server_url
+                config_dict["config"]["backend"]["baseUrl"] = sparql_endpoint
                 config_dict["config"]["backend"]["sortKey"] = 1
+                config_yaml = dict_to_yaml(config_dict)
+                log.info(f"Writing config to `{ui_config_file}`")
+                with open(ui_config_file, "w") as f:
+                    f.write(config_yaml)
             except Exception as e:
                 log.error(
                     f"Neither found config file `{ui_config_file}` nor "
@@ -172,9 +218,17 @@ class UiCommand(QleverCommand):
             return False
 
         # If we come this far, everything should work.
+        log.info("")
         log.info(
-            f"The QLever UI should now be up at {ui_url} ..."
-            f"You can log in as QLever UI admin with username and "
-            f'password "demo"'
+            f"The QLever UI should now be up at {ui_url}/{ui_config_name}"
+        )
+        log.info("")
+        log.info(
+            "You can log in as QLever UI admin with "
+            'username and password "demo"'
+        )
+        log.info(
+            f"You can modify the config file at `{ui_config_file}` "
+            f"and then just run `qlever ui` again"
         )
         return True

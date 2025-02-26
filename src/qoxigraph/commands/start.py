eb63e986421f88c0b2cefbfccd3e938445a49e2a
tanmay-9 <tanmaygarg9879@gmail.com>
2025-02-27 00:46:38 +0100

    Server container checks should be done only if args.only_show is False

--- src/qoxigraph/commands/start.py
+++ src/qoxigraph/commands/start.py
@@ -51,6 +51,28 @@ class StartCommand(start.StartCommand):
         # Check if index and server container still running
         index_container = args.index_container
         server_container = args.server_container
+
+        port = int(args.port)
+        run_subcommand = "run --restart=unless-stopped"
+        if not args.run_in_foreground:
+            run_subcommand += " -d"
+        start_cmd = "serve-read-only --location /index --bind=0.0.0.0:7878"
+        start_cmd = Containerize().containerize_command(
+            cmd=start_cmd,
+            container_system=system,
+            run_subcommand=run_subcommand,
+            image_name=args.image,
+            container_name=server_container,
+            volumes=[("$(pwd)", "/index")],
+            ports=[(port, 7878)],
+            use_bash=False,
+        )
+
+        # Show the command line.
+        self.show(start_cmd, only_show=args.show)
+        if args.show:
+            return True
+
         if Containerize().is_running(system, index_container):
             log.info(
                 f"{system} container {index_container} is still up, "
@@ -76,27 +98,6 @@ class StartCommand(start.StartCommand):
                 "if .sst index files are present in current working directory."
             )
             return False
-
-        port = int(args.port)
-        run_subcommand = "run --restart=unless-stopped"
-        if not args.run_in_foreground:
-            run_subcommand += " -d"
-        start_cmd = "serve-read-only --location /index --bind=0.0.0.0:7878"
-        start_cmd = Containerize().containerize_command(
-            cmd=start_cmd,
-            container_system=system,
-            run_subcommand=run_subcommand,
-            image_name=args.image,
-            container_name=server_container,
-            volumes=[("$(pwd)", "/index")],
-            ports=[(port, 7878)],
-            use_bash=False,
-        )
-
-        # Show the command line.
-        self.show(start_cmd, only_show=args.show)
-        if args.show:
-            return True
 
         # Run the start command.
         try:

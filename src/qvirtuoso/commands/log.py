4831f5bc3aea3c5a1bea7db06cc099f968e79bcf
tanmay-9 <tanmaygarg9879@gmail.com>
2025-03-14 10:45:32 +0100

    Change the order of commands back to index -> start for virtuoso

--- src/qvirtuoso/commands/log.py
+++ src/qvirtuoso/commands/log.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from qoxigraph.commands import log as log_cmd
+from qlever.commands import log as log_cmd
 
 
 class LogCommand(log_cmd.LogCommand):
-    def __init__(self):
-        self.script_name = "qvirtuoso"
+    def execute(self, args) -> bool:
+        args.log_file = "virtuoso.log"
+        return super().execute(args)

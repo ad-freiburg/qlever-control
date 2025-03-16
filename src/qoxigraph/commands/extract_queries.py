ada51ea01d1d6f1cfc4591d732b46d918c920e56
tanmay-9 <tanmaygarg9879@gmail.com>
2025-03-16 16:20:29 +0100

    Added symlinks for commands that are exactly the same as qlever

--- src/qoxigraph/commands/extract_queries.py
+++ src/qoxigraph/commands/extract_queries.py
@@ -1,11 +1,1 @@
-from __future__ import annotations
-
-from qlever.commands.extract_queries import (
-    ExtractQueriesCommand as QleverExtractQueriesCommand,
-)
-
-
-class ExtractQueriesCommand(QleverExtractQueriesCommand):
-    """
-    Should behave exactly the same as extract-queries command in qlever
-    """
+../../qlever/commands/extract_queries.py
--- src/qoxigraph/commands/get_data.py
+++ src/qoxigraph/commands/get_data.py
@@ -1,9 +1,1 @@
-from __future__ import annotations
-
-from qlever.commands.get_data import GetDataCommand as QleverGetDataCommand
-
-
-class GetDataCommand(QleverGetDataCommand):
-    """
-    Should behave exactly the same as get-data command in qlever
-    """
+../../qlever/commands/get_data.py

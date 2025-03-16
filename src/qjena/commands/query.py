1c11dc21f1d5806a3a8b0fe5928de171a1b2786b
tanmay-9 <tanmaygarg9879@gmail.com>
2025-03-16 16:30:35 +0100

    Added symlinks for commands that are exactly the same as in qlever and qoxigraph

--- src/qjena/commands/query.py
+++ src/qjena/commands/query.py
@@ -1,9 +1,1 @@
-from __future__ import annotations
-
-from qoxigraph.commands import query
-
-
-class QueryCommand(query.QueryCommand):
-    """
-    Should behave exactly the same as query command in qoxigraph
-    """
+../../qoxigraph/commands/query.py

3b35ebec84fc9c33c7a1f74e9236ff7d9a9d5cf9
tanmay-9 <tanmaygarg9879@gmail.com>
2025-03-22 16:46:54 +0100

    Changed default sparql-endpoint for qjena  and

--- src/qjena/commands/query.py
+++ src/qjena/commands/query.py
@@ -1,1 +1,13 @@
-../../qoxigraph/commands/query.py
+from __future__ import annotations
+
+from qoxigraph.commands.query import QueryCommand as QoxigraphQueryCommand
+
+
+class QueryCommand(QoxigraphQueryCommand):
+    def relevant_qleverfile_arguments(self) -> dict[str : list[str]]:
+        return {"data": ["name"], "server": ["port", "access_token"]}
+
+    def execute(self, args) -> bool:
+        if not args.sparql_endpoint:
+            args.sparql_endpoint = f"localhost:{args.port}/{args.name}/query"
+        super().execute(args)

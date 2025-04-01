8b2535ecf01bc19ee88a97198d01aac55a8fb96b
tanmay-9 <tanmaygarg9879@gmail.com>
2025-04-01 23:57:47 +0200

    Fix description for `start` and `status` commands for qjena

--- src/qjena/commands/status.py
+++ src/qjena/commands/status.py
@@ -5,3 +5,6 @@ from qoxigraph.commands.status import StatusCommand as QoxigraphStatusCommand
 
 class StatusCommand(QoxigraphStatusCommand):
     DEFAULT_REGEX = "fuseki-server"
+
+    def description(self) -> str:
+        return "Show Jena fuseki-server processes running on this machine"

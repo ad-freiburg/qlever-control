8d2edd988b4e845aba16673cd8d25de738f5581a
tanmay-9 <tanmaygarg9879@gmail.com>
2025-04-02 00:05:42 +0200

    Fix description and regex for `stop` and `status` commands for qblazegraph

--- src/qblazegraph/commands/status.py
+++ src/qblazegraph/commands/status.py
@@ -4,4 +4,9 @@ from qoxigraph.commands.status import StatusCommand as QoxigraphStatusCommand
 
 
 class StatusCommand(QoxigraphStatusCommand):
-    DEFAULT_REGEX = "java\\s+-server"
+    DEFAULT_REGEX = "java\\s+-server.*blazegraph.jar"
+
+    def description(self) -> str:
+        return (
+            "Show Java processes with blazegraph.jar running on this machine"
+        )

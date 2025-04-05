896a8c9010d520b06a513b3f99f2be209506c6ca
tanmay-9 <tanmaygarg9879@gmail.com>
2025-04-05 14:07:58 +0200

    Removed ui_port and added logging to qmdb `start` command

--- src/qmdb/commands/start.py
+++ src/qmdb/commands/start.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import subprocess
+import time
 from pathlib import Path
 
 from qlever.command import QleverCommand
@@ -26,7 +28,6 @@ class StartCommand(QleverCommand):
             "data": ["name"],
             "server": ["host_name", "port"],
             "runtime": ["system", "image", "server_container"],
-            "ui": ["ui_port"],
         }
 
     def additional_arguments(self, subparser):
@@ -65,14 +66,11 @@ class StartCommand(QleverCommand):
             container_name=args.server_container,
             volumes=[("$(pwd)", "/data")],
             working_directory="/data",
-            ports=[(args.port, args.port), (args.ui_port, args.ui_port)],
+            ports=[(args.port, args.port)],
         )
 
     def execute(self, args) -> bool:
-        start_cmd = (
-            f"{args.server_binary} --port {args.port} "
-            f"--browser-port {args.ui_port} index"
-        )
+        start_cmd = f"{args.server_binary} --port {args.port} index"
 
         if args.system == "native":
             if not args.run_in_foreground:
@@ -112,32 +110,56 @@ class StartCommand(QleverCommand):
 
         endpoint_url = f"http://{args.host_name}:{args.port}/sparql"
         if is_server_alive(url=endpoint_url):
-            log.error(f"MillenniumDB server already running on {endpoint_url}\n")
+            log.error(
+                f"MillenniumDB server already running on {endpoint_url}\n"
+            )
             log.info(
                 f"To kill the existing server, use `{self.script_name} stop`"
             )
             return False
 
-        # Run the start command.
         try:
-            run_command(start_cmd, show_output=True)
-            log.info(
-                f"MillenniumDB server webapp for {args.name} will be available at "
-                f"http://{args.host_name}:{args.ui_port} and the sparql endpoint for "
-                f"queries is {endpoint_url}"
+            process = run_command(
+                start_cmd,
+                use_popen=args.run_in_foreground,
             )
-            if args.run_in_foreground:
-                log.info(
-                    "Follow the log as long as the server is"
-                    " running (Ctrl-C stops the server)"
-                )
-            else:
-                log.info(
-                    f"Follow `{self.script_name} log` until the server is ready"
-                    f" (Ctrl-C stops following the log, but NOT the server)"
-                )
         except Exception as e:
-            log.error(f"Starting the MillenniumDB server failed: {e}")
+            log.error(f"Starting the MillenniumDB server failed ({e})")
             return False
+
+        # Tail the server log until the server is ready (note that the `exec`
+        # is important to make sure that the tail process is killed and not
+        # just the bash process).
+        if args.run_in_foreground:
+            log.info(
+                "Follow the server logs as long as the server is"
+                " running (Ctrl-C stops the server)"
+            )
+        else:
+            log.info(
+                "Follow the server logs until the server is ready"
+                " (Ctrl-C stops following the log, but NOT the server)"
+            )
+        log.info("")
+        log_cmd = f"exec tail -f {args.name}.server-log.txt"
+        log_proc = subprocess.Popen(log_cmd, shell=True)
+        while not is_server_alive(endpoint_url):
+            time.sleep(1)
+
+        log.info(
+            f"MillenniumDB server sparql endpoint for queries is {endpoint_url}"
+        )
+
+        # Kill the log process
+        if not args.run_in_foreground:
+            log_proc.terminate()
+
+        # With `--run-in-foreground`, wait until the server is stopped.
+        if args.run_in_foreground:
+            try:
+                process.wait()
+            except KeyboardInterrupt:
+                process.terminate()
+            log_proc.terminate()
 
         return True

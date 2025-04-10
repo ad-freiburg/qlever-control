54805b1873add156204ee5c5b61549d5baf050cd
tanmay-9 <tanmaygarg9879@gmail.com>
2025-04-10 16:53:35 +0200

    Separated stop_process_results function in `qlever stop` Useful for child classes where we have different stop logic but need the stop_process_results functionality

--- src/qlever/commands/stop.py
+++ src/qlever/commands/stop.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
+
 import re
+
 import psutil
+
 from qlever.command import QleverCommand
 from qlever.commands.status import StatusCommand
 from qlever.containerize import Containerize
 from qlever.log import log
 from qlever.util import show_process_info
+
 
 # try to kill the given process, return true iff it was killed successfully.
 # the process_info is used for logging.
@@ -45,7 +49,7 @@ class StopCommand(QleverCommand):
         pass
 
     def description(self) -> str:
-        return "Stop QLever server for a given datasedataset or port"
+        return "Stop QLever server for a given dataset or port"
 
     def should_have_qleverfile(self) -> bool:
         return True
@@ -65,6 +69,33 @@ class StopCommand(QleverCommand):
                                help="Do not look for containers, only for "
                                     "native processes")
 
+    @staticmethod
+    def stop_process_with_regex(cmdline_regex: str) -> list[bool] | None:
+        stop_process_results = []
+        for proc in psutil.process_iter():
+            try:
+                pinfo = proc.as_dict(
+                    attrs=[
+                        "pid",
+                        "username",
+                        "create_time",
+                        "memory_info",
+                        "cmdline",
+                    ]
+                )
+                cmdline = " ".join(pinfo["cmdline"])
+            except Exception as e:
+                log.debug(f"Error getting process info: {e}")
+                return None
+            if re.search(cmdline_regex, cmdline):
+                log.info(
+                    f"Found process {pinfo['pid']} from user "
+                    f"{pinfo['username']} with command line: {cmdline}"
+                )
+                log.info("")
+                stop_process_results.append(stop_process(proc, pinfo))
+        return stop_process_results
+    
     def execute(self, args) -> bool:
         # Show action description.
         cmdline_regex = args.cmdline_regex.replace("%%NAME%%", args.name)
@@ -85,21 +116,9 @@ class StopCommand(QleverCommand):
         # Check if there is a process running on the server port using psutil.
         # NOTE: On MacOS, some of the proc's returned by psutil.process_iter()
         # no longer exist when we try to access them, so we just skip them.
-        stop_process_results = []
-        for proc in psutil.process_iter():
-            try:
-                pinfo = proc.as_dict(
-                    attrs=['pid', 'username', 'create_time',
-                           'memory_info', 'cmdline'])
-                cmdline = " ".join(pinfo['cmdline'])
-            except Exception as e:
-                log.debug(f"Error getting process info: {e}")
-                return False
-            if re.search(cmdline_regex, cmdline):
-                log.info(f"Found process {pinfo['pid']} from user "
-                         f"{pinfo['username']} with command line: {cmdline}")
-                log.info("")
-                stop_process_results.append(stop_process(proc, pinfo))
+        stop_process_results = self.stop_process_with_regex(cmdline_regex)
+        if stop_process_results is None:
+            return False
         if len(stop_process_results) > 0:
             return all(stop_process_results)
 

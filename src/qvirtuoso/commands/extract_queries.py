a20afebcc81fbdfeb3c52f3fb64cf1f55d537d0e
tanmay-9 <tanmaygarg9879@gmail.com>
2025-04-17 16:33:20 +0200

    Changed get_data and extract_queries to symlinks for qvirtuoso

--- src/qvirtuoso/commands/extract_queries.py
+++ src/qvirtuoso/commands/extract_queries.py
@@ -1,9 +1,1 @@
-from __future__ import annotations
-
-from qlever.commands import extract_queries
-
-
-class ExtractQueriesCommand(extract_queries.ExtractQueriesCommand):
-    """
-    Should behave exactly the same as extract-queries command in qlever
-    """
+../../qlever/commands/extract_queries.py

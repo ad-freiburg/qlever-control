52509ad1613949b81f17a3ade922dfeb2314dd8e
Hannah Bast <bast@cs.uni-freiburg.de>
2025-04-20 23:35:02 +0200

    Add option `--group-size`

--- src/qlever/commands/update.py
+++ src/qlever/commands/update.py
@@ -39,6 +39,16 @@ class UpdateCommand(QleverCommand):
             default=self.wikidata_update_stream_url,
             help="URL of the SSE stream to update from",
         )
+        subparser.add_argument(
+            "--group-size",
+            type=int,
+            default=1,
+            help="Group this many messages together into one update "
+            "(default: one update for each message); NOTE: this simply "
+            "concatenates the `rdf_added_data` and `rdf_deleted_data` fields, "
+            "which is not 100% correct; as soon as chaining is supported, "
+            "this will be fixed",
+        )
 
     def execute(self, args) -> bool:
         # Construct the command and show it.
@@ -54,6 +64,11 @@ class UpdateCommand(QleverCommand):
         client = sseclient.SSEClient(
             args.url, headers={"Accept": "text/event-stream"}
         )
+        date_list = []
+        insert_data_list = []
+        delete_data_list = []
+        current_group_size = 0
+        curl_cmd_count = 0
         for event in client:
             # Only process non-empty messages.
             if event.event != "message" or not event.data:
@@ -73,26 +88,45 @@ class UpdateCommand(QleverCommand):
                 log.error(f"Error reading data from message: {e}")
                 continue
 
-            # Process this message.
-            log.info(
-                f"Processing operation {operation} from date {date} "
-                f"for entity {entity_id}"
-            )
+            # Adding to current group until group size is reached.
+            date_list.append(date)
+            if rdf_added_data:
+                insert_data_list.append(rdf_added_data)
+            if rdf_deleted_data:
+                delete_data_list.append(rdf_deleted_data)
+            current_group_size += 1
+            if current_group_size < args.group_size:
+                continue
+            else:
+                insert_data = "\n".join(insert_data_list)
+                delete_data = "\n".join(delete_data_list)
+                insert_data_list = []
+                delete_data_list = []
+                current_group_size = 0
+                if args.group_size == 1:
+                    log.info(
+                        f"Processing operation {operation} from date {date} "
+                        f"for entity {entity_id}"
+                    )
+                else:
+                    log.info(
+                        f"Processing group of {args.group_size} messages "
+                        f"from dates {date_list[0]} to {date_list[-1]}"
+                    )
+                date_list = []
 
             # Construct update operation.
             delete_insert_operation = (
-                f"DELETE {{"
-                f"{rdf_deleted_data if rdf_deleted_data else ''}"
-                f"}} INSERT {{"
-                f"{rdf_added_data if rdf_added_data else ''}"
-                f"}} WHERE {{ }}"
+                f"DELETE {{ {delete_data} }} "
+                f"INSERT {{ {insert_data} }} "
+                f"WHERE {{ }}"
             )
             delete_insert_operation = re.sub(
                 "\s+", " ", delete_insert_operation
             )
             # log.warn(delete_insert_operation)
 
-            # Construct and issue curl command.
+            # Construct curl command.
             sparql_endpoint = f"localhost:{args.port}"
             update_arg = f"update={shlex.quote(delete_insert_operation)}"
             access_arg = f"access-token={shlex.quote(args.access_token)}"
@@ -101,11 +135,24 @@ class UpdateCommand(QleverCommand):
                 f" --data-urlencode {update_arg}"
                 f" --data-urlencode {access_arg}"
             )
-            log.warn(curl_cmd)
+
+            # For group size 1, show the command, otherwise write to file.
+            if args.group_size == 1:
+                log.warn(curl_cmd)
+            else:
+                curl_cmd_count += 1
+                curl_cmd_file_name = f"update.curl-cmd.{curl_cmd_count}"
+                with open(curl_cmd_file_name, "w") as f:
+                    f.write(curl_cmd)
+                log.warn(f"curl command written to {curl_cmd_file_name}")
+
+            # Run it.
             try:
                 result = run_command(curl_cmd, return_output=True)
             except Exception as e:
-                log.warn(f"Error running {curl_cmd}: {e}")
+                log.warn(f"Error running curl command: {e}")
+                if args.group_size == 1:
+                    log.warn(curl_cmd)
                 continue
 
             # Results should be a JSON, parse it.
@@ -113,14 +160,16 @@ class UpdateCommand(QleverCommand):
                 result = json.loads(result)
             except Exception as e:
                 log.warn(f"Error parsing JSON result: {e}")
-                log.warn(curl_cmd)
+                if args.group_size == 1:
+                    log.warn(curl_cmd)
                 continue
 
             # Check if the result contains a QLever exception.
             if "exception" in result:
                 error_msg = result["exception"]
                 log.error(f"QLever exception: {error_msg}")
-                log.warn(curl_cmd)
+                if args.group_size == 1:
+                    log.warn(curl_cmd)
                 continue
 
             # Show statistics of the update operation.

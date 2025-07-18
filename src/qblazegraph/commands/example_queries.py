2be40798fd93c1e339d810bbd6196c83c243b2dc
tanmay-9 <tanmaygarg9879@gmail.com>
2025-07-18 14:18:48 +0200

    Add qblazegraph

---
+++ src/qblazegraph/commands/example_queries.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from qlever.commands.example_queries import (
+    ExampleQueriesCommand as QleverExampleQueriesCommand,
+)
+
+
+class ExampleQueriesCommand(QleverExampleQueriesCommand):
+    def relevant_qleverfile_arguments(self) -> dict[str : list[str]]:
+        return {"data": ["name"], "server": ["port"], "ui": ["ui_config"]}
+
+    def execute(self, args) -> bool:
+        if not args.sparql_endpoint:
+            args.sparql_endpoint = (
+                f"{args.host_name}:{args.port}/blazegraph/namespace/"
+                f"{args.name}/sparql"
+            )
+        return super().execute(args)

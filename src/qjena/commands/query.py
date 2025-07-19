0afd68a570463fb51e30d9bcc4f87027081e8a01
tanmay-9 <tanmaygarg9879@gmail.com>
2025-07-19 17:04:52 +0200

    Add missing host_name arg to qjena query

--- src/qjena/commands/query.py
+++ src/qjena/commands/query.py
@@ -5,7 +5,10 @@ from qoxigraph.commands.query import QueryCommand as QoxigraphQueryCommand
 
 class QueryCommand(QoxigraphQueryCommand):
     def relevant_qleverfile_arguments(self) -> dict[str : list[str]]:
-        return {"data": ["name"], "server": ["port", "access_token"]}
+        return {
+            "data": ["name"],
+            "server": ["port", "host_name", "access_token"],
+        }
 
     def execute(self, args) -> bool:
         if not args.sparql_endpoint:

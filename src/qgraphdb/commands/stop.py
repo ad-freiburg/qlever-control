749d06a81eb05509675efdae9956981636153475
tanmay-9 <tanmaygarg9879@gmail.com>
2025-07-30 23:35:19 +0200

    qgraphdb stop relevant_qleverfile_args fix

--- src/qgraphdb/commands/stop.py
+++ src/qgraphdb/commands/stop.py
@@ -8,6 +8,11 @@ class StopCommand(QoxigraphStopCommand):
     STATUS_COMMAND = StatusCommand()
     DEFAULT_REGEX = "com.ontotext.graphdb.server.GraphDBServer"
 
+    def relevant_qleverfile_arguments(self) -> dict[str : list[str]]:
+        return {
+            "runtime": ["system", "server_container"],
+        }
+
     def description(self) -> str:
         return "Stop the GraphDB server"
 
